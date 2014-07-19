f928e5029af607592bfb676988ce15582f78f072
GMX <moethetster@gmail.com>
2014-07-19 17:49:37 +0630

    auth endpoint

--- config.js
+++ config.js
@@ -4,6 +4,12 @@ var config = {
 
 		'groups': 'http://localhost:8000/server.php?f=groups.json',
 		'items' : 'http://localhost:8000/server.php?f=items.json',
+		/* 'users' : 'http://localhost:8000/server.php?f=users.json',*/
+
+		'auth': {
+			'method': 'POST',
+			'url': 'http://localhost:8000/server.php?f=auth.json'
+		},
 
 		'order' : {
 
