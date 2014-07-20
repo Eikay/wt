e57ca19ac4171d917e77f6f6d8984ce2d0501be3
GMX <moethetster@gmail.com>
2014-07-20 11:38:07 +0630

    change some data source for evaluation without endpoint server

---
+++ groups.js
@@ -0,0 +1,28 @@
+var groups = [
+	{
+		"id": 1,
+		"name": "Foods" 
+	},
+
+	{
+		"id": 2,
+		"name": "Drinks"
+	},
+
+	{
+		"id": 3,
+		"name": "Fries"
+	},
+
+	{
+		"id": 4,
+		"name": "Boils"
+	},
+
+	{
+		"id": 5,
+		"name": "Soups"
+	}
+];
+
+App.storage( "groups", groups );
---
+++ items.js
@@ -0,0 +1,36 @@
+var items = [
+	{
+		"MID": 1,
+		"Name": "Kong Baung (Chicken)",
+		"CusPrice": 1000,
+		"CusPrice1": 900,
+		"GroupId": 1
+	},
+
+	{
+		"MID": 2,
+		"Name": "Kong Baung (Pork)",
+		"CusPrice": 1200,
+		"CusPrice1": 900,
+		"GroupId": 1
+	},
+
+	{
+		"MID": 3,
+		"Name": "Kyar San Kyaw",
+		"CusPrice": 1500,
+		"CusPrice1": 1200,
+		"groupId": 1
+	},
+
+	{
+		"MID": 4,
+		"Name": "Fried Noodle",
+		"CusPrice": 1800,
+		"CusPrice1": 1200,
+		"GroupId": 1
+	}
+]
+
+
+App.storage( "items", items );
