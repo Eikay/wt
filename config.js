5832ac046c4fc9fe4ea0866a1b417632f8c7def6
GMX <moethetster@gmail.com>
2014-09-07 00:58:44 +0630

    update config according to server side implementation

--- config.js
+++ config.js
@@ -7,18 +7,18 @@ var config = {
 
 	'endPoints': {
 
-		'groups': 'http://localhost:8000/server.php?f=groups.json',
-		'items' : 'http://localhost:8000/server.php?f=items.json',
+		'groups': 'http://localhost:3000/groups',
+		'items' : 'http://localhost:3000/items',
 		/* 'users' : 'http://localhost:8000/server.php?f=users.json',*/
 
 		'auth': {
 			'method': 'POST',
-			'url': 'http://localhost:8000/server.php?f=auth.json'
+			'url': 'http://localhost:3000/auth'
 		},
 
 		'order' : {
 
-			'url'   : 'http://localhost:8000/server.php?f=order.json',
+			'url'   : 'http://localhost:3000/order',
 			'method': 'POST'	
 
 		}
